5798247169f8c2d4651c598bd53925a9d480509d
Фуад Джавадов <fuad.dzhavadov@idp.zyfra.com>
2025-06-13 16:31:34 +0300

    chore: add taiga provideEventPlugins

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {AuthService} from '../services/auth.service';
 import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import { provideEventPlugins} from '@taiga-ui/event-plugins';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimations(),
+    provideAnimationsAsync(),
+    provideEventPlugins(),
     AuthService,
   ]
 };
