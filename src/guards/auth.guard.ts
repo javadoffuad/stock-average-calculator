ad29c7e74afd886fb25b73262e025fb9e02b4299
fuad.dzhavadov1 <fuad.dzhavadov@idp.zyfra.com>
2025-06-15 16:09:04 +0300

    refactor: update routes

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,13 +1,14 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {AuthService} from '../services/auth.service';
+import {ROUTES} from '../constants/routes.constants';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (!authService.getAccessToken()) {
-    router.navigate(['/access-token']);
+    router.navigate([`/${ROUTES.auth}`]);
     return false;
   }
   return true;
