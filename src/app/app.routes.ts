07e2eb2d56408ee4f8d43e318f5e797127d118bb
fuad.dzhavadov1 <fuad.dzhavadov@idp.zyfra.com>
2025-06-15 16:07:24 +0300

    chore: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import {authGuard} from '../guards/auth.guard';
-import {PAGE_POSITIONS_PARAM} from '../constants/routes.constants';
+import {PAGE_POSITIONS_PARAM, ROUTES} from '../constants/routes.constants';
 
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: ROUTES.home,
+  },
+  {
+    path: ROUTES.home,
     canActivate: [authGuard],
     loadComponent: () => import('../components/positions-page/positions-page.component').then(m => m.PositionsPageComponent),
     children: [
-
       {
         path: `:${PAGE_POSITIONS_PARAM}`,
         loadComponent: () =>
@@ -23,12 +27,12 @@ export const routes: Routes = [
     ],
   },
   {
-    path: 'calculator',
+    path: ROUTES.calculator,
     loadComponent: () =>
       import('../components/calculator/calculator.component').then(m => m.CalculatorComponent),
   },
   {
-    path: 'access-token',
+    path: ROUTES.auth,
     loadComponent: () => import('../components/access-token/access-token.component').then(m => m.AccessTokenComponent),
   },
 ];
--- src/constants/routes.constants.ts
+++ src/constants/routes.constants.ts
@@ -1,7 +1,7 @@
 export const ROUTES = {
   calculator: 'calculator',
   auth: 'access-token',
-  home: '/',
+  home: 'positions',
 }
 
 export const PAGE_POSITIONS_PARAM = 'ticker';
