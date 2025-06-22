d2c2eeec6f3c95be4d59dd7a313ce070f65f409d
fuad.dzhavadov1 <fuad.dzhavadov@idp.zyfra.com>
2025-06-22 14:47:29 +0300

    refactor: styling position item

--- src/components/position-item/position-item.component.ts
+++ src/components/position-item/position-item.component.ts
@@ -1,12 +1,27 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, input, Signal} from '@angular/core';
 import {IPosition} from '../../models/operation.models';
 import {combinePrice} from '../../utils/currency.utils';
 import {RouterLink} from '@angular/router';
+import {TuiCard} from '@taiga-ui/layout';
+import {TuiAppearance, TuiAutoColorPipe, TuiInitialsPipe, TuiTitle} from '@taiga-ui/core';
+import {TuiAvatar} from '@taiga-ui/kit';
+import {TuiAmountPipe, TuiCurrencyPipe} from '@taiga-ui/addon-commerce';
+import {AsyncPipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-position-item',
   imports: [
-    RouterLink
+    RouterLink,
+    TuiCard,
+    TuiAppearance,
+    TuiTitle,
+    TuiAvatar,
+    TuiAutoColorPipe,
+    TuiInitialsPipe,
+    TuiCurrencyPipe,
+    UpperCasePipe,
+    TuiAmountPipe,
+    AsyncPipe,
   ],
   templateUrl: './position-item.component.html',
   styleUrl: './position-item.component.css'
@@ -14,4 +29,5 @@ import {RouterLink} from '@angular/router';
 export class PositionItemComponent {
   public position = input.required<IPosition>();
   protected readonly combinePrice = combinePrice;
+  protected readonly quantityUnits: Signal<number> = computed(() => Number(this.position().quantity.units));
 }
--- src/utils/currency.utils.ts
+++ src/utils/currency.utils.ts
@@ -5,8 +5,8 @@
  * @example Пример использования:
  * const amount = combinePrice('0', 126300000); // Результат: 0.1263
  */
-export function combinePrice(units: string | number, nano: number): number {
-  const unitsNum = typeof units === 'string' ? parseInt(units, 10) : units;
+export function combinePrice(units: string, nano: number): number {
+  const unitsNum = parseInt(units, 10);
   const sign = Math.sign(unitsNum) || 1; // Учитываем знак (если units = 0, берём знак nano)
   const nanoAdjusted = sign * Math.abs(nano) / 1e9; // Делим на 1 миллиард (10^9)
   return unitsNum + nanoAdjusted;
