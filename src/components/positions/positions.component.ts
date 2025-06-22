3acac13d9fa428a66f38a0d0e9ddc565c3ec59fe
fuad.dzhavadov1 <fuad.dzhavadov@idp.zyfra.com>
2025-06-22 18:16:23 +0300

    chore: load instruments

--- src/components/positions/positions.component.ts
+++ src/components/positions/positions.component.ts
@@ -1,7 +1,8 @@
-import {Component, computed, inject} from '@angular/core';
+import {Component, computed, effect, inject} from '@angular/core';
 import {PositionItemComponent} from '../position-item/position-item.component';
 import {OperationsService} from '../../services/operations.service';
 import {TuiTitle} from "@taiga-ui/core";
+import {InstrumentsService} from '../../services/instruments/instruments.service';
 
 @Component({
   selector: 'app-positions',
@@ -14,8 +15,19 @@ import {TuiTitle} from "@taiga-ui/core";
 })
 export class PositionsComponent {
   private readonly operationsService = inject(OperationsService);
+  private readonly instrumentsService = inject(InstrumentsService);
 
   protected portfolio = this.operationsService.selectPortfolio;
   protected positions = computed(() => this.portfolio()?.positions);
-  protected positionShares = computed(() => this.positions()?.filter(p => p.instrumentType === 'share'))
+  protected positionShares = computed(() => this.positions()?.filter(p => p.instrumentType === 'share') || [])
+
+  constructor() {
+    effect(() => {
+      const positionShareIds = this.positionShares().map(p => p.instrumentUid);
+
+      if (positionShareIds.length > 0) {
+        this.instrumentsService.loadInstrumentsBy(positionShareIds);
+      }
+    });
+  }
 }
--- src/services/instruments/instruments.service.ts
+++ src/services/instruments/instruments.service.ts
@@ -3,6 +3,7 @@ import {API_URL} from '../../constants/api.constants';
 import {HttpClient} from '@angular/common/http';
 import {AuthService} from '../auth.service';
 import {IInstrument, IInstrumentResponse} from '../../models/instrument.models';
+import {forkJoin, Observable, take} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,12 @@ export class InstrumentsService {
   private http = inject(HttpClient);
   private authService = inject(AuthService);
   private instrument = signal<IInstrument | null>(null);
+  private instruments = signal<Record<string, IInstrument | null>>({});
   public selectInstrument = computed(() => this.instrument());
 
   constructor() { }
 
-  loadInstrumentBy(id: string, idType= 'INSTRUMENT_ID_TYPE_UID', classCode = ''){
+  public loadInstrumentBy(id: string, idType= 'INSTRUMENT_ID_TYPE_UID', classCode = ''){
     this.http.post<IInstrumentResponse>(
       `${this.serviceUrl}ShareBy`,
       {
@@ -30,6 +32,46 @@ export class InstrumentsService {
           'Authorization': `Bearer ${this.authService.getAccessToken()}`,
         }
       }
-    ).subscribe(response => this.instrument.set(response.instrument));
+    ).pipe(take(1)).subscribe(response => this.instrument.set(response.instrument));
+  }
+
+  public loadInstrumentsBy(ids: string[]): void{
+    if (!ids.length) {
+      return;
+    }
+    const requests$ = ids.map(id => this.loadShare(id));
+
+    forkJoin(requests$).pipe(take(1)).subscribe(responses => {
+      const newInstruments: Record<string, IInstrument> = {};
+      for (const {instrument} of responses) {
+        newInstruments[instrument.uid] = instrument;
+      }
+
+      this.instruments.update(currentInstruments => ({
+        ...currentInstruments,
+        ...newInstruments,
+      }));
+    });
+  }
+
+  public selectInstrumentById(instrumentId: string): IInstrument | null {
+    return this.instruments()[instrumentId] || null;
+  }
+
+  private loadShare(id: string, idType= 'INSTRUMENT_ID_TYPE_UID', classCode = ''): Observable<IInstrumentResponse> {
+    return this.http.post<IInstrumentResponse>(
+      `${this.serviceUrl}ShareBy`,
+      {
+        idType,
+        id,
+        classCode,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.authService.getAccessToken()}`,
+        }
+      }
+    );
   }
 }
