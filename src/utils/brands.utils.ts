031e8a7f38b6f5a69e004be12d541f0c2b9185a1
fuad.dzhavadov1 <fuad.dzhavadov@idp.zyfra.com>
2025-06-29 16:00:44 +0300

    refactor: optimization load images

--- src/utils/brands.utils.ts
+++ src/utils/brands.utils.ts
@@ -1,6 +1,6 @@
 import {API_BRANDS_URL} from '../constants/api.constants';
 
 export function getFullLogoUrl(brand: {logoName: string}): string {
-  const logoName = brand.logoName.replace('.png', 'x640.png')
+  const logoName = brand.logoName.replace('.png', 'x160.png')
   return API_BRANDS_URL + logoName;
 }
